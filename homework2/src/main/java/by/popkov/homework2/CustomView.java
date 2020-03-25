2f1b372a49c9d33474cfdcd8b142823f182ff281
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-03-25 16:20:22 +0300

    up

--- homework2/src/main/java/by/popkov/homework2/CustomView.java
+++ homework2/src/main/java/by/popkov/homework2/CustomView.java
@@ -4,12 +4,14 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Rect;
 import android.graphics.RectF;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+
 
 public class CustomView extends View {
     private Paint paintLeftBottom;
@@ -18,7 +20,9 @@ public class CustomView extends View {
     private Paint paintRightBottom;
     private Paint paintCenter;
     private int bigCircleRadius = 400;
-    RectF rectf;
+    private int smallCircleRadius = 150;
+    Context myContext;
+    Canvas myCanvas;
 
 
     int centerX;
@@ -26,6 +30,7 @@ public class CustomView extends View {
 
     public CustomView(Context context) {
         super(context);
+        myContext = context;
         initPaintRightBottom();
         initPaintLeftBottom();
         initPaintLeftTop();
@@ -35,6 +40,7 @@ public class CustomView extends View {
 
     public CustomView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        myContext = context;
         initPaintRightBottom();
         initPaintLeftBottom();
         initPaintLeftTop();
@@ -44,6 +50,7 @@ public class CustomView extends View {
 
     public CustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        myContext = context;
         initPaintRightBottom();
         initPaintLeftBottom();
         initPaintLeftTop();
@@ -53,6 +60,7 @@ public class CustomView extends View {
 
     public CustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+        myContext = context;
         initPaintRightBottom();
         initPaintLeftBottom();
         initPaintLeftTop();
@@ -61,7 +69,60 @@ public class CustomView extends View {
     }
 
     @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        invalidate();
+        float eventX = event.getX();
+        float eventY = event.getY();
+        colorChange(eventX, eventY);
+        String pos = String.format("Нажаты координаты %f:%f", eventX, eventY);
+        Toast.makeText(myContext, String.valueOf(getRandomColor()), Toast.LENGTH_SHORT).show();
+        paintLeftTop.setColor(getRandomColor());
+        myCanvas.drawArc(centerX - bigCircleRadius, centerY - bigCircleRadius,
+                centerX + bigCircleRadius, centerY + bigCircleRadius,
+                180, 90, true, paintLeftTop);
+        return super.onTouchEvent(event);
+    }
+
+    private void colorChange(float eventX, float eventY) {
+        if (eventX > centerX - bigCircleRadius && eventX < centerX) {
+            if (eventY > centerY - bigCircleRadius && eventY < centerY) {
+                paintLeftTop.setColor(getRandomColor());
+            }
+//            }else if (eventY < centerY + bigCircleRadius && eventY > centerY){}
+        }
+    }
+
+    private int getRandomColor() {
+        switch ((int) (Math.random() * 10)) {
+            case 0:
+                return Color.BLACK;
+            case 1:
+                return Color.DKGRAY;
+            case 2:
+                return Color.GRAY;
+            case 3:
+                return Color.LTGRAY;
+            case 4:
+                return Color.RED;
+            case 5:
+                return Color.GREEN;
+            case 6:
+                return Color.BLUE;
+            case 7:
+                return Color.YELLOW;
+            case 8:
+                return Color.CYAN;
+            case 9:
+                return Color.MAGENTA;
+            default:
+                return Color.WHITE;
+        }
+    }
+
+
+    @Override
     protected void onDraw(Canvas canvas) {
+        myCanvas = canvas;
         canvas.drawArc(centerX - bigCircleRadius, centerY - bigCircleRadius,
                 centerX + bigCircleRadius, centerY + bigCircleRadius,
                 0, 90, true, paintRightBottom);
@@ -78,7 +139,7 @@ public class CustomView extends View {
                 centerX + bigCircleRadius, centerY + bigCircleRadius,
                 270, 90, true, paintRightTop);
 
-        canvas.drawCircle(centerX, centerY, 150, paintCenter);
+        canvas.drawCircle(centerX, centerY, smallCircleRadius, paintCenter);
 
         super.onDraw(canvas);
     }
--- homework2/src/main/java/by/popkov/homework2/MainActivity.java
+++ homework2/src/main/java/by/popkov/homework2/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_custom_view);
+        setContentView(R.layout.activity_main);
         startActivity(new Intent(this, CustomActivity.class));
     }
 }
