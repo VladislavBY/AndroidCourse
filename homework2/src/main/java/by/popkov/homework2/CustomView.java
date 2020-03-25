73ee1b36637626eee892253f916e916effb50425
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-03-25 12:38:14 +0300

    up

--- homework2/src/main/java/by/popkov/homework2/CustomView.java
+++ homework2/src/main/java/by/popkov/homework2/CustomView.java
@@ -10,23 +10,34 @@ import android.view.View;
 import androidx.annotation.Nullable;
 
 public class CustomView extends View {
-//    private Paint paint;
+    private Paint paint;
+
+    int centerX;
+    int centerY;
 
     public CustomView(Context context) {
         super(context);
+        initPaint();
 
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        canvas.drawColor(Color.GREEN);
+        canvas.drawCircle(centerX, centerY, 100, paint);
         super.onDraw(canvas);
     }
 
-//    private void initPaint() {
-//        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-//        paint.setColor(Color.BLUE);
-//        paint.setAntiAlias(true);
-//        paint.setStyle(Paint.Style.FILL);
-//    }
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        centerX = w / 2;
+        centerY = h / 2;
+        super.onSizeChanged(w, h, oldw, oldh);
+    }
+
+    private void initPaint() {
+        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setColor(Color.BLUE);
+        paint.setAntiAlias(true);
+        paint.setStyle(Paint.Style.FILL);
+    }
 }
