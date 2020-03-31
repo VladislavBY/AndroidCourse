f48d2bad5393bdef832718c352ee81915f24beb5
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-03-31 15:33:00 +0300

    up

---
+++ homework3/src/main/java/by/popkov/homework3/AddContact.java
@@ -0,0 +1,20 @@
+package by.popkov.homework3;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddContact extends AppCompatActivity {
+    public static Intent newIntent (Context context){
+        return new Intent(context, AddContact.class);
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_contact);
+    }
+}
--- homework3/src/main/java/by/popkov/homework3/ContactsActivity.java
+++ homework3/src/main/java/by/popkov/homework3/ContactsActivity.java
@@ -1,13 +1,29 @@
 package by.popkov.homework3;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class ContactsActivity extends AppCompatActivity {
+
+    private FloatingActionButton floatingActionButtonAddContact;
+
+    private int requestCode = 7777777;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_contact);
+        setContentView(R.layout.activity_contacts);
+
+        floatingActionButtonAddContact = findViewById(R.id.floatingActionButtonAddContact);
+        floatingActionButtonAddContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(AddContact.newIntent(ContactsActivity.this), requestCode);
+            }
+        });
     }
 }
