b588ab459bc273d6076637fb762db9baade60bb1
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-03-31 16:20:53 +0300

    up

--- homework3/src/main/java/by/popkov/homework3/AddContact.java
+++ homework3/src/main/java/by/popkov/homework3/AddContact.java
@@ -3,12 +3,25 @@ package by.popkov.homework3;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddContact extends AppCompatActivity {
-    public static Intent newIntent (Context context){
+    private Button buttonBack;
+    private Button buttonAdd;
+
+    private RadioButton radioButtonPhoneNumber;
+    private RadioButton radioButtonEmail;
+
+    private EditText editTextName;
+    private EditText editTextPhoneNumberOrEmail;
+
+
+    public static Intent newIntent(Context context) {
         return new Intent(context, AddContact.class);
     }
 
