5cd5d81fca3351c97bd1b2d3897af0def292fbb1
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-03-31 16:57:40 +0300

    up

--- homework3/src/main/java/by/popkov/homework3/AddContact.java
+++ homework3/src/main/java/by/popkov/homework3/AddContact.java
@@ -3,16 +3,16 @@ package by.popkov.homework3;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.RadioButton;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddContact extends AppCompatActivity {
-    private Button buttonBack;
-    private Button buttonAdd;
+    private ImageButton buttonBack;
+    private ImageButton buttonAdd;
 
     private RadioButton radioButtonPhoneNumber;
     private RadioButton radioButtonEmail;
@@ -29,6 +29,15 @@ public class AddContact extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_contact);
-        setResult(RESULT_OK);
+        viewInit();
+    }
+
+    private void viewInit() {
+        buttonBack = findViewById(R.id.buttonBack);
+        buttonAdd = findViewById(R.id.buttonAdd);
+        radioButtonPhoneNumber = findViewById(R.id.radioButtonPhoneNumber);
+        radioButtonEmail = findViewById(R.id.radioButtonEmail);
+        editTextName = findViewById(R.id.editTextName);
+        editTextPhoneNumberOrEmail = findViewById(R.id.editTextPhoneNumberOrEmail);
     }
 }
