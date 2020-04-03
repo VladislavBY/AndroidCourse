20643bb7696ff4f6d574457073bb92a9b92fb784
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-04-03 17:06:18 +0300

    up

--- homework3/src/main/java/by/popkov/homework3/ContactsActivity.java
+++ homework3/src/main/java/by/popkov/homework3/ContactsActivity.java
@@ -10,7 +10,6 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
