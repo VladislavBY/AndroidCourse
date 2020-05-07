44d1562f8528cb0be117dfab67719be69a684b23
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-05-07 10:51:35 +0300

    hw 8. rebuild

--- homework8/src/main/java/by/popkov/homework8/MainFragment.java
+++ homework8/src/main/java/by/popkov/homework8/MainFragment.java
@@ -1,12 +1,17 @@
 package by.popkov.homework8;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -23,8 +28,7 @@ import okhttp3.Request;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
 
-
-public class MainActivity extends AppCompatActivity {
+class MainFragment extends Fragment {
     private static final String API_WEATHER_NOW = "https://samples.openweathermap.org/data/2.5/find?q=%s&units=%s&appid=%s";
     private static final String API_KEY = "a179821de4f14533abfde5b6ae9204b0";
     static final String UNITS_IMPERIAL = "imperial";
@@ -37,19 +41,24 @@ public class MainActivity extends AppCompatActivity {
     private TextView tempTextView;
     private TextView weatherMainTextView;
 
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_main);
-        initViews();
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initViews(view);
         showWeatherNow("London", UNITS_METRIC);
     }
 
-    private void initViews() {
-        weatherMainImageView = findViewById(R.id.weatherMainImageView);
-        cityTextView = findViewById(R.id.cityTextView);
-        tempTextView = findViewById(R.id.tempTextView);
-        weatherMainTextView = findViewById(R.id.weatherMainTextView);
+    private void initViews(View view) {
+        weatherMainImageView = view.findViewById(R.id.weatherMainImageView);
+        cityTextView = view.findViewById(R.id.cityTextView);
+        tempTextView = view.findViewById(R.id.tempTextView);
+        weatherMainTextView = view.findViewById(R.id.weatherMainTextView);
     }
 
     private void showWeatherNow(final String cityName, String units) {
@@ -59,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
         okHttpClient.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(@NotNull Call call, @NotNull IOException e) {
-                Toast.makeText(MainActivity.this, getString(R.string.no_connection), Toast.LENGTH_SHORT)
+                Toast.makeText(getContext(), getString(R.string.no_connection), Toast.LENGTH_SHORT)
                         .show();
             }
 
@@ -71,12 +80,14 @@ public class MainActivity extends AppCompatActivity {
                     Type type = new TypeToken<WeatherApi>() {
                     }.getType();
                     final WeatherApi weatherNow = new Gson().fromJson(json, type);
-                    new Handler(getMainLooper()).post(new Runnable() {
-                        @Override
-                        public void run() {
-                            setWeatherView(weatherNow, cityName);
-                        }
-                    });
+                    if (getContext() != null) {
+                        new Handler(getContext().getMainLooper()).post(new Runnable() {
+                            @Override
+                            public void run() {
+                                setWeatherView(weatherNow, cityName);
+                            }
+                        });
+                    }
                 }
             }
         });
@@ -87,6 +98,6 @@ public class MainActivity extends AppCompatActivity {
         tempTextView.setText(String.valueOf(weatherNow.getWeatherApiList().get(0).getWeatherApiListMain().getTemp()));
         weatherMainTextView.setText(weatherNow.getWeatherApiList().get(0).getWeatherApiListWeather().get(0).getMain());
         String icon = weatherNow.getWeatherApiList().get(0).getWeatherApiListWeather().get(0).getIcon();
-        weatherMainImageView.setImageResource(getResources().getIdentifier("weather" + icon, "drawable", getPackageName()));
+        weatherMainImageView.setImageResource(getResources().getIdentifier("weather" + icon, "drawable", getContext().getPackageName()));
     }
 }
