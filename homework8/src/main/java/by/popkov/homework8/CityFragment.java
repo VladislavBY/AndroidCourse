886d0b9e06bdc906fd63b514c6888563f08ed150
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-05-08 12:27:41 +0300

    hw 8 add settings

---
+++ homework8/src/main/java/by/popkov/homework8/CityFragment.java
@@ -0,0 +1,23 @@
+package by.popkov.homework8;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class CityFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_city, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
