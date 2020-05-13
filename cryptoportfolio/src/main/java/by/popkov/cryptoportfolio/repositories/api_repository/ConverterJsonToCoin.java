f00ecf481151bbb571d7edd4149ed6e09fa30908
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-05-13 17:38:54 +0300

    Course project start

---
+++ cryptoportfolio/src/main/java/by/popkov/cryptoportfolio/repositories/api_repository/ConverterJsonToCoin.java
@@ -0,0 +1,11 @@
+package by.popkov.cryptoportfolio.repositories.api_repository;
+
+import java.util.List;
+
+import by.popkov.cryptoportfolio.Coin;
+
+public class ConverterJsonToCoin {
+    public static List<Coin> toCoin(String json) {
+        return null;
+    }
+}
