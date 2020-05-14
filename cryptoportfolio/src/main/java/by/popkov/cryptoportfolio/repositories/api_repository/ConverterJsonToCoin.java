e7c7e8b4a693163733338fb59a953423812df278
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-05-14 15:45:51 +0300

    Course project. Converter Json

--- cryptoportfolio/src/main/java/by/popkov/cryptoportfolio/repositories/api_repository/ConverterJsonToCoin.java
+++ cryptoportfolio/src/main/java/by/popkov/cryptoportfolio/repositories/api_repository/ConverterJsonToCoin.java
@@ -1,11 +1,37 @@
 package by.popkov.cryptoportfolio.repositories.api_repository;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import by.popkov.cryptoportfolio.Coin;
 
 public class ConverterJsonToCoin {
-    public static List<Coin> toCoin(String json) {
-        return null;
+    public static List<Coin> toCoin(List<String> symbols, List<Double> numbers, String json) {
+        List<Coin> coinsList = new ArrayList<>();
+        try {
+            JSONObject jsonObject = new JSONObject(json);
+            JSONObject raw = jsonObject.getJSONObject("RAW");
+            for (int i = 0; i < symbols.size(); i++) {
+                String symbol = symbols.get(i);
+                JSONObject jsonCoin = raw.getJSONObject(symbol);
+                JSONObject usd = jsonCoin.getJSONObject("USD");
+                double price = usd.getDouble("PRICE");
+                Double number = numbers.get(i);
+                Coin coin = new Coin.Builder(symbol)
+                        .setLogoUrl("https://www.cryptocompare.com" + usd.getString("IMAGEURL"))
+                        .setPrise(price)
+                        .setNumber(number)
+                        .setChangePercent24Hour(usd.getDouble("CHANGEPCT24HOUR"))
+                        .setSum(number * price)
+                        .build();
+                coinsList.add(coin);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return coinsList;
     }
 }
