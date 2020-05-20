d5e39bdcbb292a320d73b70db918b3de0277e6d9
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-05-20 14:40:58 +0300

    Course project.

---
+++ cryptoportfolio/src/main/java/by/popkov/cryptoportfolio/my_portfolio_viewer/CoinForView.java
@@ -0,0 +1,100 @@
+package by.popkov.cryptoportfolio.my_portfolio_viewer;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import by.popkov.cryptoportfolio.Coin;
+
+class CoinForView {
+    @NonNull
+    private String symbol;
+    @Nullable
+    private String logoUrl;
+    @Nullable
+    private String fiatSymbol;
+    @Nullable
+    private String prise;
+    @Nullable
+    private String changePercent24Hour;
+    @Nullable
+    private String change24Hour;
+    @Nullable
+    private String number;
+    @Nullable
+    private String sum;
+    @Nullable
+    private String globalSupply;
+    @Nullable
+    private String marketCap;
+    @Nullable
+    private String market24Volume;
+
+    @NonNull
+    public String getSymbol() {
+        return symbol;
+    }
+
+    @Nullable
+    public String getLogoUrl() {
+        return logoUrl;
+    }
+
+    @Nullable
+    public String getFiatSymbol() {
+        return fiatSymbol;
+    }
+
+    @Nullable
+    public String getPrise() {
+        return prise;
+    }
+
+    @Nullable
+    public String getChangePercent24Hour() {
+        return changePercent24Hour;
+    }
+
+    @Nullable
+    public String getChange24Hour() {
+        return change24Hour;
+    }
+
+    @Nullable
+    public String getNumber() {
+        return number;
+    }
+
+    @Nullable
+    public String getSum() {
+        return sum;
+    }
+
+    @Nullable
+    public String getGlobalSupply() {
+        return globalSupply;
+    }
+
+    @Nullable
+    public String getMarketCap() {
+        return marketCap;
+    }
+
+    @Nullable
+    public String getMarket24Volume() {
+        return market24Volume;
+    }
+
+    public CoinForView(Coin coin) {
+        this.symbol = coin.getSymbol();
+        this.logoUrl = coin.getLogoUrl();
+        this.fiatSymbol = coin.getFiatSymbol();
+        this.prise = String.valueOf(coin.getPrise());
+        this.changePercent24Hour = String.valueOf(coin.getChangePercent24Hour());
+        this.change24Hour = String.valueOf(coin.getChange24Hour());
+        this.number = String.valueOf(coin.getNumber());
+        this.sum = String.valueOf(coin.getSum());
+        this.globalSupply = String.valueOf(coin.getGlobalSupply());
+        this.marketCap = String.valueOf(coin.getMarketCap());
+        this.market24Volume = String.valueOf(coin.getMarket24Volume());
+    }
+}
