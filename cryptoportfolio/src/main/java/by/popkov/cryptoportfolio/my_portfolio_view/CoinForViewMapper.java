71d41b109d4876dfede56f0db32fe8849a463a4f
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-05-28 13:11:09 +0300

    Course project. Fix

--- cryptoportfolio/src/main/java/by/popkov/cryptoportfolio/my_portfolio_view/CoinForViewMapper.java
+++ cryptoportfolio/src/main/java/by/popkov/cryptoportfolio/my_portfolio_view/CoinForViewMapper.java
@@ -1,25 +1,28 @@
 package by.popkov.cryptoportfolio.my_portfolio_view;
 
-
+import java.util.Locale;
 import java.util.function.Function;
 
 import by.popkov.cryptoportfolio.domain.Coin;
 
 public class CoinForViewMapper implements Function<Coin, CoinForView> {
+    private static final String numberFormat = "%.2f";
+
     @Override
     public CoinForView apply(Coin coin) {
+        Locale locale = Locale.getDefault();
         CoinForView.Builder builder = new CoinForView.Builder(coin.getSymbol());
         builder.setLogoUrl(coin.getLogoUrl())
                 .setFiatSymbol(coin.getFiatSymbol())
-                .setPrise(String.valueOf(coin.getPrise()))
-                .setChangePercent24Hour(String.valueOf(coin.getChangePercent24Hour()))
-                .setChange24Hour(String.valueOf(coin.getChange24Hour()))
-                .setNumber(String.valueOf(coin.getNumber()))
-                .setGlobalSupply(String.valueOf(coin.getGlobalSupply()))
-                .setMarketCap(String.valueOf(coin.getMarketCap()))
-                .setMarket24Volume(String.valueOf(coin.getMarket24Volume()));
+                .setPrise(String.format(locale, numberFormat, coin.getPrise()))
+                .setChangePercent24Hour(String.format(locale, numberFormat, coin.getChangePercent24Hour()) + "%")
+                .setChange24Hour(String.format(locale, numberFormat, coin.getChange24Hour()))
+                .setNumber(String.format(locale, numberFormat, coin.getNumber()))
+                .setGlobalSupply(String.format(locale, numberFormat, coin.getGlobalSupply()))
+                .setMarketCap(String.format(locale, numberFormat, coin.getMarketCap()))
+                .setMarket24Volume(String.format(locale, numberFormat, coin.getMarket24Volume()));
         if (coin.getNumber() != null && coin.getPrise() != null) {
-            builder.setSum(String.valueOf(coin.getNumber() * coin.getPrise()));
+            builder.setSum("Sum: " + String.format(locale, numberFormat, coin.getNumber() * coin.getPrise()));
         } else {
             builder.setSum("null");
         }
