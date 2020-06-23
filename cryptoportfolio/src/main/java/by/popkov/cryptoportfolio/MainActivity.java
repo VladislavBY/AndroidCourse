fb0591ec9ddacc10eb825991a3c82f11bb849f88
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-06-23 17:10:29 +0300

    Project. Basic animation done

--- cryptoportfolio/src/main/java/by/popkov/cryptoportfolio/MainActivity.java
+++ cryptoportfolio/src/main/java/by/popkov/cryptoportfolio/MainActivity.java
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 import by.popkov.cryptoportfolio.coin_info_fragment.CoinInfoFragment;
 import by.popkov.cryptoportfolio.data_classes.CoinForView;
 import by.popkov.cryptoportfolio.my_portfolio_fragment.CoinListAdapter;
 import by.popkov.cryptoportfolio.my_portfolio_fragment.MyPortfolioFragment;
 import by.popkov.cryptoportfolio.settings_fragment.SettingsFragment;
+
+import static android.view.animation.AnimationUtils.*;
 
 public class MainActivity extends AppCompatActivity implements CoinListAdapter.OnCoinListClickListener,
         OnHomeClickListener, MyPortfolioFragment.OnSettingsBtnClickListener, SettingsFragment.OnUpdateCoinListListener {
@@ -18,6 +21,7 @@ public class MainActivity extends AppCompatActivity implements CoinListAdapter.O
     @Override
     public void onHomeClick() {
         getSupportFragmentManager().popBackStack();
+        startAnimScreenChangeBackward();
     }
 
     @Override
@@ -26,6 +30,7 @@ public class MainActivity extends AppCompatActivity implements CoinListAdapter.O
                 .add(R.id.fragmentContainer, SettingsFragment.getInstance(), SettingsFragment.TAG)
                 .addToBackStack(null)
                 .commit();
+        startAnimScreenChangeForward();
     }
 
     @Override
@@ -34,6 +39,7 @@ public class MainActivity extends AppCompatActivity implements CoinListAdapter.O
                 .add(R.id.fragmentContainer, CoinInfoFragment.getInstance(coinForView), CoinInfoFragment.TAG)
                 .addToBackStack(null)
                 .commit();
+        startAnimScreenChangeForward();
     }
 
     @Override
@@ -44,6 +50,8 @@ public class MainActivity extends AppCompatActivity implements CoinListAdapter.O
         }
     }
 
+    private FrameLayout fragmentContainer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,12 +59,20 @@ public class MainActivity extends AppCompatActivity implements CoinListAdapter.O
         if (savedInstanceState == null) {
             showMyPortfolioFragment();
         }
-
+        fragmentContainer = findViewById(R.id.fragmentContainer);
     }
 
     private void showMyPortfolioFragment() {
         getSupportFragmentManager().beginTransaction()
                 .add(R.id.fragmentContainer, MyPortfolioFragment.getInstance(), MyPortfolioFragment.TAG)
                 .commit();
+    }
+
+    private void startAnimScreenChangeForward() {
+        fragmentContainer.startAnimation(loadAnimation(this, R.anim.screen_change_forward));
+    }
+
+    private void startAnimScreenChangeBackward() {
+        fragmentContainer.startAnimation(loadAnimation(this, R.anim.screen_change_backward));
     }
 }
